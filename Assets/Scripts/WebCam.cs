a792367bdb6557bb4cefc3e9a8edd69c8e061572
Sandesh Agawane <sndshagawane412@gmail.com>
2018-07-10 21:43:06 +0530

    Initial commit

---
+++ Assets/Scripts/WebCam.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WebCam : MonoBehaviour {
+
+    public RawImage rawImage;
+	// Use this for initialization
+	void Start () {
+        WebCamDevice[] devices = WebCamTexture.devices;
+        for (int i = 0; i < devices.Length;i++){
+            Debug.Log("Webcam available :" + devices[i].name);
+        }
+        WebCamTexture realtimefeed  = new WebCamTexture();
+        rawImage.texture = realtimefeed;
+        rawImage.material.mainTexture = realtimefeed;
+        realtimefeed.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
